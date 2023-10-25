868a1ce68ee7f2282edfb174dd5ed5c6ee31b8d4
Abhay Tiwari <abhayskp7948@gmail.com>
2023-10-25 15:45:10 +0530

    Prepared a user model

---
+++ src/models/user.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    email: {
+      type : DataTypes.STRING,
+      allowNull : false,
+      unique : true,
+      validate : {
+        isEmail : true
+      }
+    },
+    password: {
+      type : DataTypes.STRING,
+      validate : {
+        // password must have more than 3 character and < 50 character
+        len : [3,50]
+    }
+  }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
