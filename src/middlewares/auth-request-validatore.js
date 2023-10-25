c3f9707cbc5e3c59030b635dd0f819cd95d3f638
Abhay Tiwari <abhayskp7948@gmail.com>
2023-10-26 00:00:23 +0530

    Added email and password validation for incoming request

---
+++ src/middlewares/auth-request-validatore.js
@@ -0,0 +1,15 @@
+const validateUserSingup = (req,res,next) => {
+    if(!req.body.email || !req.body.password ){
+        return res.status(400).json({
+            success : false,
+            data : {},
+            message : 'Something went wrong',
+            err : 'Email or password missing in the signup logic'
+        })
+    }
+    next();
+}
+
+module.exports = {
+    validateUserSingup
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const UserController = require('../../controllers/user-controller');
+const { AuthRequestValidator } = require('../../middlewares/index');
 const router = express.Router();
 
-router.post('/signup', UserController.create);
-router.post('/signin' , UserController.singin);
+router.post('/signup', AuthRequestValidator.validateUserSingup , UserController.create);
+router.post('/signin' , AuthRequestValidator.validateUserSingup, UserController.singin);
 module.exports = router;
